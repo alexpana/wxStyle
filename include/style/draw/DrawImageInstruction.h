ead028e89fad28fe78750a1e8c8dc53a5f10a596
Alexandru Pana <astronothing@gmail.com>
2014-05-06 22:52:44 +0300

    Added DrawImageInstruction

--- include/style/draw/DrawImageInstruction.h
+++ include/style/draw/DrawImageInstruction.h
@@ -3,6 +3,7 @@
 #include <wx/bitmap.h>
 
 #include "DimPoint.h"
+#include "style/draw/Anchor.h"
 #include "style/draw/IDrawInstruction.h"
 
 namespace wxstyle {
@@ -14,27 +15,33 @@ namespace wxstyle {
         public:
             Builder& SetImagePath(const wxString& imagePath);
             Builder& SetPosition(const DimPoint& position);
+            Builder& SetHorizontalAnchor(HorizontalAnchor horizontalAnchor);
+            Builder& SetVerticalAnchor(VerticalAnchor verticalAnchor);
+            Builder& SetImageSize(const DimPoint& size);
 
             DrawImageInstruction Build();
 
         private:
             Builder();
-            Builder(const wxString& imagePath, const DimPoint& position);
+            Builder(const wxString& imagePath, const DimPoint& position, HorizontalAnchor horizontalAnchor, VerticalAnchor verticalAnchor, const DimPoint& size);
 
         private:
             wxString m_imagePath;
             DimPoint m_position;
+            DimPoint m_imageSize;
+            HorizontalAnchor m_horizontalAnchor;
+            VerticalAnchor m_verticalAnchor;
         };
 
     public:
-        DrawImageInstruction(const wxString& imagePath, const DimPoint& position);
+        DrawImageInstruction(const wxString& imagePath, const DimPoint& position, HorizontalAnchor horizontalAnchor, VerticalAnchor verticalAnchor, const DimPoint& size);
 
         static Builder NewBuilder() {
             return Builder();
         }
 
         Builder ToBuilder() {
-            return Builder(m_imagePath, m_position);
+            return Builder(m_imagePath, m_position, m_horizontalAnchor, m_verticalAnchor, m_imageSize);
         }
 
         wxString GetImagePath() const {
@@ -50,6 +57,11 @@ namespace wxstyle {
     private:
         const wxString m_imagePath;
         const DimPoint m_position;
+        HorizontalAnchor m_horizontalAnchor;
+        VerticalAnchor m_verticalAnchor;
+        DimPoint m_imageSize;
+
+        // used for caching the bitmap
         const boost::optional<wxBitmap> m_cachedBitmap;
     };
 } // namespace wxstyle
--- src/style/draw/DrawImageInstruction.cpp
+++ src/style/draw/DrawImageInstruction.cpp
@@ -1,14 +1,19 @@
 #include "style/draw/DrawImageInstruction.h"
 
+#include "style/draw/ImageRepository.h"
+
 #include <wx/bitmap.h>
 
 namespace wxstyle {
 
     typedef DrawImageInstruction::Builder Builder;
 
-    Builder::Builder() {}
-    Builder::Builder(const wxString& imagePath, const DimPoint& position) :
-        m_imagePath(imagePath), m_position(position)
+    Builder::Builder() : 
+        m_imagePath(""), m_position(DimPoint(0, 0)), m_horizontalAnchor(HA_CENTER), m_verticalAnchor(VA_CENTER), m_imageSize(DimPoint(Dimension(0, 1), Dimension(0, 1)))
+    {}
+
+    Builder::Builder(const wxString& imagePath, const DimPoint& position, HorizontalAnchor horizontalAnchor, VerticalAnchor verticalAnchor, const DimPoint& size) :
+        m_imagePath(imagePath), m_position(position), m_horizontalAnchor(horizontalAnchor), m_verticalAnchor(verticalAnchor), m_imageSize(size)
     {}
 
     Builder& Builder::SetImagePath(const wxString& imagePath) {
@@ -21,16 +26,47 @@ namespace wxstyle {
         return *this;
     }
 
+    Builder& Builder::SetHorizontalAnchor(HorizontalAnchor horizontalAnchor) {
+        m_horizontalAnchor = horizontalAnchor;
+        return *this;
+    }
+
+    Builder& Builder::SetVerticalAnchor(VerticalAnchor verticalAnchor) {
+        m_verticalAnchor = verticalAnchor;
+        return *this;
+    }
+
+    Builder& Builder::SetImageSize(const DimPoint& size) {
+        m_imageSize = size;
+        return *this;
+    }
+
     DrawImageInstruction Builder::Build() {
-        return DrawImageInstruction(m_imagePath, m_position);
+        return DrawImageInstruction(m_imagePath, m_position, m_horizontalAnchor, m_verticalAnchor, m_imageSize);
     }
 
 
-    DrawImageInstruction::DrawImageInstruction(const wxString& imagePath, const DimPoint& position) :
-        m_imagePath(imagePath), m_position(position)
+    DrawImageInstruction::DrawImageInstruction(const wxString& imagePath, const DimPoint& position, HorizontalAnchor horizontalAnchor, VerticalAnchor verticalAnchor, const DimPoint& size) :
+        m_imagePath(imagePath), m_position(position), m_horizontalAnchor(horizontalAnchor), m_verticalAnchor(verticalAnchor), m_imageSize(size)
     {}
 
     void DrawImageInstruction::Draw(wxGraphicsContext* g, const wxSize& windowSize) const {
+
+        wxImage *image = ImageRepository::GetInstance()->GetImage(m_imagePath);
+
+        if (image == nullptr) {
+            return;
+        }
+
+        wxBitmap bitmap = wxBitmap(*image);
+
+
+        wxPoint imagePosition(m_position.GetValue(windowSize).GetWidth(), m_position.GetValue(windowSize).GetHeight());
+        wxSize imageSize(m_imageSize.GetValue(image->GetSize()));
+
+        wxPoint offset = ComputeOffset(imageSize, m_horizontalAnchor, m_verticalAnchor);
+
+        g->DrawBitmap(bitmap, imagePosition.x + offset.x, imagePosition.y + offset.y, imageSize.GetWidth(), imageSize.GetHeight());
     }
 
 } // namespace wxstyle
