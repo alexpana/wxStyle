0ad76a85e3b8d8063c47245b6127cd72b7fb8af5
Alexandru Pana <astronothing@gmail.com>
2014-05-06 20:56:55 +0300

    Added fixture to ImageRepositoryTests to ensure proper cleanup

--- tests/ImageRepositoryTests.cpp
+++ tests/ImageRepositoryTests.cpp
@@ -12,21 +12,33 @@ BOOST_AUTO_TEST_SUITE(image_repository_suite)
 using namespace wxstyle;
 using namespace std;
 
+class TestFixture {
+public:
+    TestFixture() {
+        wxImage::AddHandler(new wxJPEGHandler());
+
+        ImageRepository *repository = ImageRepository::GetInstance();
+
+        repository->SetRepositoryPath(wxGetCwd() + wxFileName::GetPathSeparator() + "data");
+    }
+
+    ~TestFixture() {
+        ImageRepository::ReleaseInstance();
+        wxImage::CleanUpHandlers();
+    }
+};
+
 BOOST_AUTO_TEST_CASE(load_image) {
-    wxImage::AddHandler(new wxJPEGHandler());
+
+    TestFixture fixture;
 
     ImageRepository *repository = ImageRepository::GetInstance();
-
-    repository->SetRepositoryPath(wxGetCwd() + wxFileName::GetPathSeparator() + "data");
 
     wxImage *testImage = repository->GetImage("test_image.jpg");
 
     BOOST_REQUIRE(testImage != nullptr);
     BOOST_CHECK_EQUAL(testImage->GetWidth(), 8);
     BOOST_CHECK_EQUAL(testImage->GetHeight(), 8);
-
-    ImageRepository::ReleaseInstance();
-    wxImage::CleanUpHandlers();
 }
 
 BOOST_AUTO_TEST_SUITE_END()
